8185eaa63dbbcd76b3262e5532b1d96c0230f61e
htrgouvea <hi@heitorgouvea.me>
2023-10-21 16:00:18 -0300

    applying funcional programming principals

--- scripts/slack_webhook.py
+++ scripts/slack_webhook.py
@@ -3,34 +3,33 @@
 import requests
 import argparse
 
-webhook = ''
-
-def message(data):
+def message(data, webhook):
     try:
         payload = {"text": data}
         headers = {"Content-type": "application/json"}
         response = requests.post(webhook, json=payload, headers=headers)
 
         if response.status_code == 200:
-            print(f"Message sent successfully! [{response.text}]")
-        else:
-            print(f"Failed to send message: [{response.status_code}]")
+            return f"Message sent successfully! [{response.text}]"
+
+        return f"Failed to send message: [{response.status_code}]"
+    
     except requests.exceptions.RequestException as e:
-        print(f"Error sending message: {str(e)}")
+        return f"Error sending message: {str(e)}"
 
 
 def main():
     parser = argparse.ArgumentParser(description="An output forwarder script for webhooks.")
     parser.add_argument("-m", "--message", help="Enter what you want to send.\n\ne.g. slack.py --message/-m \"something[...]\"", required=True)
+    parser.add_argument('--webhook', help='Set the webhook adress', required=True)
+
     args = parser.parse_args()
 
     if args.message:
         try:
-            message(args.message)
-        except KeyboardInterrupt:
-            exit(1)
+            return message(args.message, args.webhook)
         except EOFError:
             exit(1)
 
 if __name__ == '__main__':
-    main()
+    print (main())
