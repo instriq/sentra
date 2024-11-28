ba8026f5c57f8bfe21c32f4aa61a92135e924cba
htrgouvea <hi@heitorgouvea.me>
2024-11-28 18:37:36 +0000

    update helper menu

--- lib/Sentra/Utils/Helper.pm
+++ lib/Sentra/Utils/Helper.pm
@@ -11,11 +11,11 @@ package Sentra::Utils::Helper {
             \r\t-------                         -----------
             \r\t-o, --org                       Specify the name of the organization
             \r\t-t, --token                     Set the GitHub Token to use during actions
-            \r\t-w, --webhook                   Set the webhook address for Slack
-            \r\t-m, --message                   Message to send via Slack webhook
             \r\t-mt, --maintained               Check last commit date of repositories
             \r\t-d, --dependency                Check for dependabot.yaml file in repositories
-            \r\t-p, --per_page                  Set the number of items per page in API requests (default: 100)\n\n";
+            \r\t-M, --metrics                   See some metrics based on GHAS
+            \r\t-w, --webhook                   Set the webhook address for Slack
+            \r\t-m, --message                   Message to send via Slack webhook\n\n";
     }
 }
 
