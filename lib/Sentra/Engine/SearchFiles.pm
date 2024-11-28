ca1e3af65b673a3e0416f68c0bfa2f261d9bbf8b
htrgouvea <hi@heitorgouvea.me>
2024-11-28 21:34:25 +0000

    better code composition

--- lib/Sentra/Engine/SearchFiles.pm
+++ lib/Sentra/Engine/SearchFiles.pm
@@ -15,27 +15,27 @@ package Sentra::Engine::SearchFiles {
             'X-GitHub-Api-Version' => '2022-11-28'
         };
 
-        my $output = '';
-
+        my $output   = '';
         my $repo_url = "https://api.github.com/orgs/$org/repos?per_page=$per_page";
-        my $repo_tx = $userAgent -> get($repo_url => $headers);
+        my $repo_tx  = $userAgent -> get($repo_url => $headers);
 
-        my $res = $repo_tx->result or return "Error fetching repositories: " . $repo_tx->error->{message} . "\n";
-        $res->is_success or return "Error fetching repositories: " . $res->message . "\n";
-
-        my $repos = $res->json;
+        my $res = $repo_tx -> result();
+        
+        if ($res -> is_success) {
+            my $repos = $res -> json();
        
-        for my $repo (@$repos) {
-            next if $repo -> {archived};
-            
-            my $full_name = "$org/$repo->{name}";
+            for my $repo (@$repos) {
+                next if $repo -> {archived};
+                
+                my $full_name = "$org/$repo->{name}";
 
-            my $dependabot_url = "https://api.github.com/repos/$full_name/contents/.github/dependabot.yaml";
-            my $dependabot_tx = $userAgent->get($dependabot_url => $headers);
-            
-            if ($dependabot_tx->result->code == 404) {
-                $output .= "The dependabot.yml file was not found in this repository: https://github.com/$full_name\n";
-            }   
+                my $dependabot_url = "https://api.github.com/repos/$full_name/contents/.github/dependabot.yaml";
+                my $dependabot_tx = $userAgent -> get($dependabot_url => $headers);
+                
+                if ($dependabot_tx -> result -> code == 404) {
+                    $output .= "The dependabot.yml file was not found in this repository: https://github.com/$full_name\n";
+                }  
+            }
         }
 
         return $output || "No issues found.";
