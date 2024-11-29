bbbe48b60c7e25c5015d424cb4252cba275d4989
htrgouvea <hi@heitorgouvea.me>
2024-11-29 12:42:03 +0000

    new modules to optmize

--- lib/Sentra/Utils/UserAgent.pm
+++ lib/Sentra/Utils/UserAgent.pm
@@ -1,19 +1,27 @@
 package Sentra::Utils::UserAgent {
     use strict;
     use warnings;
-    use Mojo::UserAgent;
+    use LWP::UserAgent;
 
     sub new {
-        my $userAgent = Mojo::UserAgent -> new();
+        my ($self, $token) = @_;
+
+        my $userAgent = LWP::UserAgent -> new(
+            timeout  => 5,
+            ssl_opts => { 
+                verify_hostname => 0,
+                SSL_verify_mode => 0
+            },
+            agent => "Sentra 0.0.3"
+        );
         
-        my $headers = {
+        $userAgent -> default_headers -> header (
             'X-GitHub-Api-Version' => '2022-11-28',
             'Accept'               => 'application/vnd.github+json',
-            'User-Agent'           => 'Sentra 0.0.3',
             'Authorization'        => "Bearer $token"
-        };
+        );
 
-        return "";
+        return $userAgent;
     }
 }
 
