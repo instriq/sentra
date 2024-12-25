3a4c27ef99ac95c2bd02f5029054bb07400743fc
htrgouvea <hi@heitorgouvea.me>
2024-12-25 15:27:29 +0300

    fix linter warning

--- lib/Sentra/Utils/Helper.pm
+++ lib/Sentra/Utils/Helper.pm
@@ -3,19 +3,21 @@ package Sentra::Utils::Helper {
     use warnings;
 
     sub new {
-        return "
-            \rSentra v0.0.3
-            \rCore Commands
-            \r==============
-            \r\tCommand                         Description
-            \r\t-------                         -----------
-            \r\t-o, --org                       Specify the name of the organization
-            \r\t-t, --token                     Set the GitHub Token to use during actions
-            \r\t-mt, --maintained               Get alerts about repositories with a last commit date greater than 90 days old
-            \r\t-d, --dependency                Check if repositories has dependabot.yaml file
-            \r\t-M, --metrics                   See some metrics based on GHAS
-            \r\t-w, --webhook                   Set the webhook address for Slack
-            \r\t-m, --message                   Message to send via Slack webhook\n\n";
+        return join("\n",
+            "Sentra v0.0.3",
+            "Core Commands",
+            "    ==============",
+            "        Command                         Description",
+            "        -------                         -----------",
+            "        -o, --org                       Specify the name of the organization",
+            "        -t, --token                     Set the GitHub Token to use during actions",
+            "        -mt, --maintained               Get alerts about repositories with a last commit date greater than 90 days old",
+            "        -d, --dependency                Check if repositories has dependabot.yaml file",
+            "        -M, --metrics                   See some metrics based on GHAS",
+            "        -w, --webhook                   Set the webhook address for Slack",
+            "        -m, --message                   Message to send via Slack webhook",
+            ""
+        );
     }
 }
 
